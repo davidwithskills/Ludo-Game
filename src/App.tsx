e2a984949b24d3450dbe8982b1c0a98fddb6b2b1
David Yusuf <davidwithskills@gmail.com>
2026-06-03 16:47:42 +0100

    Add GitHub Pages base path to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Ludo-Game/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
